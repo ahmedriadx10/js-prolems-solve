49dfd50c8c6f47abfaa0650d1719d27aa48bf4c7
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-02-04 18:48:51 +0600

    problem solved

--- solve-11.js
+++ solve-11.js
@@ -77,8 +77,6 @@ function analyzeMarks(marksData) {
 const result2 = analyzeMarks(marks);
 // console.log(result2);
 
-
-
 //Problem 3: Password Strength Checker
 
 //Function name: checkPassword(password)
@@ -91,84 +89,206 @@ const result2 = analyzeMarks(marks);
 
 // Must not contain spaces
 
+const testData = "hel ";
+const testData2 = "idKhullam3";
 
-const testData="hel "
-const testData2='idKhullam3'
+function passwordStrenchChecker(getPassword) {
+  let hasNumber = 0;
+  let hasUpper = 0;
+  const reasons = [];
 
+  const splitString = getPassword.split("");
 
-function passwordStrenchChecker(getPassword){
-
-let hasNumber=0;
-let hasUpper=0;
-const reasons=[]
-
-
-const splitString=getPassword.split('')
-
-
-if(getPassword.length<8){
-  reasons.push('password char less than 8')
-}
-
-
-for(const x of splitString){
-
-  if(Number(x)>=0 && Number(x)<=9){
-
-    hasNumber++
+  if (getPassword.length < 8) {
+    reasons.push("password char less than 8");
   }
 
-  if(x>='A' && x<='Z'){
+  for (const x of splitString) {
+    if (Number(x) >= 0 && Number(x) <= 9) {
+      hasNumber++;
+    }
 
-    hasUpper++
+    if (x >= "A" && x <= "Z") {
+      hasUpper++;
+    }
   }
 
+  if (getPassword.includes(" ")) {
+    reasons.push("blank space found");
+  }
 
+  if (hasNumber === 0) {
+    reasons.push("No Number Found");
+  } else if (hasUpper === 0) {
+    reasons.push("No Upper Case Found");
+  }
 
+  return {
+    valid: reasons.length === 0,
+    reasons,
+  };
 }
 
+const reuslt3 = passwordStrenchChecker(testData2);
 
-if(getPassword.includes(' ')){
-  reasons.push('blank space found')
+// console.log(reuslt3);
+
+// Problem 4: Shopping Bill Calculator
+
+//Statement:  Calculate total bill amount and count how many times each item appears.
+
+//Input:
+
+// Output:
+// {
+//   total: 376,
+//   itemCount: { egg: 3, rice: 1, oil: 1, sugar: 1 }
+// }
+
+const prices = { rice: 70, oil: 180, egg: 12, sugar: 90 };
+const items = ["egg", "Egg", "rice", "oil", "eGg", "sugar", "sugar"];
+
+function calcBill(priceData, itemsData) {
+  const returnData = {
+    total: 0,
+    itemCount: {},
+  };
+
+  for (const p in priceData) {
+    returnData.total += priceData[p];
+  }
+
+  for (const i of itemsData) {
+    const lowCaseData = i.toLowerCase();
+
+    if (returnData["itemCount"].hasOwnProperty(lowCaseData)) {
+      returnData.itemCount[lowCaseData]++;
+    } else {
+      returnData.itemCount[lowCaseData] = 1;
+    }
+  }
+
+  // console.log(returnData);
 }
 
+const result4 = calcBill(prices, items);
+// console.log(result4);
 
-if(hasNumber===0){
-  reasons.push('No Number Found')
+const prices2 = { pen: 10, book: 50 };
+const items2 = ["pen", "pen", "book", "pen"];
+
+const result5 = calcBill(prices2, items2);
+// console.log(result5)
+
+// A developer take problems and breakdown then write code
+
+//Problem 5: Remove Duplicates From Array
+
+// Return a new array where duplicate values are removed, keeping the original order.
+
+function removeDuplicates(data) {
+  const unicData = [];
+
+  for (const u of data) {
+    if (!unicData.includes(u)) {
+      unicData.push(u);
+    }
+  }
+
+  return unicData;
 }
 
-else if (hasUpper===0){
-  reasons.push('No Upper Case Found')
+const numbers = [5, 3, 5, 2, 3, 9, 2, 7];
+
+const result6 = removeDuplicates(numbers);
+// console.log(result6)
+
+//Problem 6: Phonebook Prefix Search
+
+//  Return an array of names that start with the given prefix.
+
+function searchByPrefix(phooneBook, prefix) {
+  const returnData = [];
+
+  for (const x in phooneBook) {
+    if (x.startsWith(prefix)) {
+      returnData.push(x);
+    }
+  }
+
+  return returnData;
 }
 
+const phonebook = {
+  rahim: "0181",
+  karim: "0172",
+  rafi: "0193",
+  raju: "017",
+  riad: "018",
+};
+const prefix = "ra";
 
+const result7 = searchByPrefix(phonebook, prefix);
+// console.log(result7)
 
+const phonebook3 = { mina: "013", mim: "014", rina: "015" };
+const prefix3 = "mi";
 
+const result8 = searchByPrefix(phonebook3, prefix3);
+// console.log(result8)
 
-return {
+//  Problem 7: Text Stats Generator
 
-  valid:reasons.length===0,
-reasons
+//Statement:
+//  Return an object containing:
+// Characters count excluding spaces
 
+// Words count
+
+// Vowels count
+
+// Consonants count
+
+function textStats(txt) {
+  const vowels = "aeiou";
+  let countVowel = 0;
+  let spaceCount = 0;
+
+  const cutSideSpace = txt.trim();
+
+  const splitText = cutSideSpace.split("");
+
+  for (const t of splitText) {
+    const lowerTcase = t.toLowerCase();
+
+    if (vowels.includes(lowerTcase)) {
+      countVowel++;
+    }
+
+    if (t === " ") {
+      spaceCount++;
+    }
+  }
+
+  const textWords = cutSideSpace.split(" ");
+
+  const consonantsData = splitText.length - spaceCount - countVowel;
+
+  const returnData = {
+    characters: splitText.length - spaceCount,
+    words: textWords.length,
+    vowels: countVowel,
+    consonants: consonantsData,
+  };
+
+  return returnData;
 }
 
+const txtData = " JavaScript is fun to learn ";
+const txtData2 = " I am OK";
 
-}
-
-
-
-const reuslt3=passwordStrenchChecker(testData2)
-
-console.log(reuslt3)
-
-
-
-
-
-
-
-
-
+const result9 = textStats(txtData);
+// console.log(result9);
 
 
 
