65ea3ce2f236600450599ae7b4e104ca340ac234
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-02-14 14:41:15 +0600

    dom simple solve added

---
+++ js-dom-practice/solve.js
@@ -0,0 +1,50 @@
+const students = [
+    { name: "Riad", marks: 85, city: "Dhaka" },
+    { name: "Ahmed", marks: 72, city: "Chittagong" },
+    { name: "Sifat", marks: 92, city: "Rajshahi" },
+    { name: "Tanvir", marks: 60, city: "Sylhet" },
+    { name: "Junaid", marks: 78, city: "Khulna" }
+];
+
+
+
+const mainSection=document.getElementById('student-data')
+mainSection.classList.add('container')
+
+for(const x of students){
+
+  // data main div
+  const div=document.createElement('div')
+div.innerHTML=`
+<h2>${x.name}</h2>
+<p>Marks: ${x.marks}</p>
+<p>City: ${x.city}</p>
+`
+
+if(x.marks>80){
+
+div.style.border='3px solid gold'
+const p=document.createElement('p')
+div.appendChild(p)
+p.innerText='ScholarShip Candidate'
+p.style.fontStyle='italic'
+p.style.fontWeight='bold'
+
+}
+else{
+  div.style.border='3px solid gray'
+}
+
+
+
+
+
+//used css wrote style for save time
+div.classList.add('card')
+  
+mainSection.appendChild(div)
+
+
+
+
+}
